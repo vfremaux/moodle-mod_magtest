2a618692b6b5bf258fdc2cfc226aa12c0299450b
valery <valery.fremaux@gmail.com>
2016-04-07 14:31:42 +0200

    upgrade mod form

--- mod_form.php
+++ mod_form.php
@@ -38,11 +38,17 @@ class mod_magtest_mod_form extends moodleform_mod {
         $mform =& $this->_form;
 
         $mform->addElement('header', 'general', get_string('general', 'form'));
-        $mform->addElement('text', 'name', get_string('name'), array('size'=>'64'));
-        $mform->setType('name', PARAM_CLEANHTML);
-        $mform->addRule('name', null, 'required', null, 'client');
 
-        $this->add_intro_editor(true, get_string('intro', 'magtest'));
+        $mform->addElement('text', 'name', get_string('name'), array('size'=>'64'));
+        if (!empty($CFG->formatstringstriptags)) {
+            $mform->setType('name', PARAM_TEXT);
+        } else {
+            $mform->setType('name', PARAM_CLEANHTML);
+        }
+        $mform->addRule('name', null, 'required', null, 'client');
+        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
+
+        $this->standard_intro_elements();
 
         $startdatearray[] = &$mform->createElement('date_time_selector', 'starttime', '');
         $startdatearray[] = &$mform->createElement('checkbox', 'starttimeenable', '');
