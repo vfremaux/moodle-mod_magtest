1732121e97e1384cba9f87e336434646dc4129a3
valery <valery.fremaux@gmail.com>
2016-06-15 11:11:30 +0200

    fixes controller processing order that breaks last page recording

--- version.php
+++ version.php
@@ -19,18 +19,17 @@
  * This fragment is called by moodle_needs_upgrading() and /admin/index.php
  *
  * @author Valery Fremaux
- * @version $Id: version.php,v 1.2 2012-11-01 18:05:14 vf Exp $
  * @package mod_magtest
  * @category mod
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015053000;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2016060100;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2014110400;  // Requires this Moodle version
 $plugin->cron     = 0;           // Period for cron to check this module (secs)
 $plugin->component = 'mod_magtest';   // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.8.0 (Build 2014012802)';
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '2.8.0 (Build 2016060100)';
 
 
