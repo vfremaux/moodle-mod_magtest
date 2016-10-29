2305c51be1e892953b6c37f0257b229d55565ca1
valery <valery.fremaux@gmail.com>
2016-10-30 01:29:44 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -15,22 +15,21 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Code fragment to define the version of magtest
- * This fragment is called by moodle_needs_upgrading() and /admin/index.php
+ * Version details.
  *
- * @author Valery Fremaux
- * @version $Id: version.php,v 1.2 2012-11-01 18:05:14 vf Exp $
  * @package mod_magtest
  * @category mod
+ * @author Valery Fremaux
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2016060100;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2015111100;  // Requires this Moodle version
-$plugin->cron     = 0;           // Period for cron to check this module (secs)
 $plugin->component = 'mod_magtest';   // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.0.0 (Build 2016060100)';
 
+// Non Moodle attributes.
+$plugin->codeincrement = '3.0.0000';
 
