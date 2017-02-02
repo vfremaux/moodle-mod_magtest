24e482ee360023da107acf427a44240817d35af8
valery <valery.fremaux@gmail.com>
2017-02-02 21:49:44 +0100

    add verison 3.2

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016060100;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2016052300;  // Requires this Moodle version
-$plugin->component = 'mod_magtest';   // Full name of the plugin (used for diagnostics)
+$plugin->version  = 2016060100;  // The current module version (Date: YYYYMMDDXX).
+$plugin->requires = 2016052300;  // Requires this Moodle version.
+$plugin->component = 'mod_magtest';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1.0 (Build 2016060100)';
+$plugin->release = '3.2.0 (Build 2016060100)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.2.0000';
