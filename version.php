7e1cee9f73d359f3d0ac872f7f5ca99eee58f48a
valery <valery.fremaux@gmail.com>
2019-09-09 11:11:04 +0200

    Feed distribution center

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018112800;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires = 2018112800;  // Requires this Moodle version.
+$plugin->requires = 2019051100;  // Requires this Moodle version.
 $plugin->component = 'mod_magtest';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.6.0 (Build 2018112800)';
+$plugin->release = '3.7.0 (Build 2018112800)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.6.0001';
+$plugin->codeincrement = '3.7.0001';
 $plugin->privacy = 'public';
