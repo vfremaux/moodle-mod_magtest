689bf17d39e8f09918e486c9d3e8abe65dedbc4c
valery <valery.fremaux@gmail.com>
2019-09-12 19:03:57 +0200

    Travis fix

--- mod_form.php
+++ mod_form.php
@@ -84,7 +84,8 @@ class mod_magtest_mod_form extends moodleform_mod {
         $mform->addElement('checkbox', 'allowreplay', get_string('allowreplay', 'magtest'));
         $mform->addHelpButton('allowreplay', 'pagesize', 'magtest');
 
-        $mform->addElement('htmleditor', 'result', get_string('resulttext', 'magtest'));
+        $resultoptions = [];
+        $mform->addElement('editor', 'result', get_string('resulttext', 'magtest'), $resultoptions);
         $mform->setType('result', PARAM_RAW);
 
         $this->standard_coursemodule_elements();
